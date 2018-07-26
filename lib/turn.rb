cff8beba07acba4eee3d1849912d3bb77dfb4522
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-07-26 19:00:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -58,7 +58,8 @@ def turn(board)
     puts "Can't move here"
     puts "Where else would you like to move?"
     input = gets.strip
-    index = input_to_index
+    index = input_to_index(input)
+    
     
     
 end
